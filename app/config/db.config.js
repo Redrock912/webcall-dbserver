930923823c4bc1234c97b551168a5ae28b795c94
Redrock912 <leset912@gmail.com>
2019-11-14 16:57:39 +0900

    added db config info

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "us-cdbr-iron-east-05.cleardb.net",
-  USER: "bf30a3c05fa183",
-  PASSWORD: "8086d028",
-  DB: "heroku_858b2c7635f5aef"
+  USER: "b418b0955e872a",
+  PASSWORD: "4ac06b08",
+  DB: "heroku_790b9a5749f51c3"
 };
 
 // module.exports = {
